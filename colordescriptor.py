494e2271246d4e3aad26d31ab396d1efc74816fd
bdowe <briandowe@gmail.com>
2020-06-06 14:33:43 -0700

    Finish describe function

--- colordescriptor.py
+++ colordescriptor.py
@@ -22,3 +22,27 @@ class ColorDescriptor:
 		axesX, axesY = int(w * 0.75) // 2, int(h * 0.75) // 2 
 		ellipMask = np.zeros(image.shape[:2], dtype="uint8")
 		cv2.ellipse(ellipMask, (cX, cY), (axesX, axesY), 0, 0, 360, 255, -1)
+
+		for (startX, endX, startY, endY) in segments:
+			# construct mask for each quadrant of image, subtracting elliptical mask from it
+			cornerMask = np.zeros(image.shape[:2], dtype="uint8")
+			cv2.rectangle(cornerMask, (startX, startY), (endX, endY), 255, -1)
+			cornerMask = cv2.subtract(cornerMask, ellipMask)
+
+			# extract color histogram and update feature vector
+			hist = self.histogram(image, cornerMask)
+			features.extend(hist)
+
+		# extract color histogram from elliptical region and update feature vector
+		hist = self.histogram(image, ellipMask)
+		features.extend(hist)
+
+		return features
+
+	def histogram(image):
+		pass
+
+
+
+
+		
