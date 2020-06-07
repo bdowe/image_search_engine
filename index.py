5bacf4aa4aadaa4b1758acef3e1b8fba54d44847
bdowe <briandowe@gmail.com>
2020-06-06 21:58:57 -0700

    Finalize core functions for index.puy

--- index.py
+++ index.py
@@ -8,4 +8,22 @@ ap.add_argument("-d", "--dataset", required=True, help="Path to directory contai
 ap.add_argument("-i", "--index", required=True, help="Path to where computed index will be stored")
 args = vars(ap.parse_args())
 
-cd = ColorDescriptor(8, 12, 3)
+cd = ColorDescriptor((8, 12, 3))
+
+output = open(args["index"], "w")
+
+# use glob to loop over image paths
+for imagePath in glob.glob(args["dataset"] + "/*.png"):
+	# extract image ID (unique filename) from image
+	imageID = imagePath[imagePath.rfind("/") + 1:]
+	image = cv2.imread(imagePath)
+
+	# describe image and extract features
+	features = cd.describe(image)
+
+	# write features to output file
+	feaures = [str(f) for f in features]
+	output.write("%s,%s\n" % (imageID, ",".join(features)))
+
+output.close()
+
